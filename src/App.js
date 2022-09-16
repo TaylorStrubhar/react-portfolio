cec7ffae761fa608e979bcd5da2e44ec6f151765
Ira Strubhar <taylor.strubhar@gmail.com>
2022-09-16 00:21:01 -0700

    adjusted categories

--- src/App.js
+++ src/App.js
@@ -7,13 +7,10 @@ import Footer from './components/Footer';
 
 function App() {
   const [categories] = useState([
-    {
-      name: 'commercial',
-      description: 'Photos of grocery stores, food trucks, and other commercial projects',
-    },
-    { name: 'portraits', description: 'Portraits of people in my life' },
-    { name: 'food', description: 'Delicious delicacies' },
-    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+    { name: 'about me', description: 'Get to know me better' },
+    { name: 'portfolio', description: 'A listing of my work' },
+    { name: 'contact', description: 'My contact information' },
+    { name: 'resume', description: 'View my qualifications and work history' },
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
