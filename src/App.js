d13edafb26f98d14e3764da1ec0486b646918c6d
Ira Strubhar <taylor.strubhar@gmail.com>
2022-09-18 18:10:06 -0700

    various components added and updated

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
-  const [contactSelected, setContactSelected] = useState(false);
+  const [aboutSelected, setAboutSelected] = useState(false);
 
   return (
     <div>
@@ -24,12 +24,12 @@ function App() {
                     categories={categories}
                     setCurrentCategory={setCurrentCategory}
                     currentCategory={currentCategory}
-                    contactSelected={contactSelected}
-                    setContactSelected={setContactSelected}
+                    aboutSelected={aboutSelected}
+                    setAboutSelected={setAboutSelected}
             ></Nav>
         </div>
       <main>
-        {!contactSelected ? (
+        {!aboutSelected ? (
           <>
             <Project currentCategory={currentCategory}></Project>
             <About></About>
